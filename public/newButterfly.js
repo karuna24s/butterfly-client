b600397d15437e10fb890a6baf4cc66d857ff310
Katharine Francis <katie-francis@hotmail.com>
2020-04-09 19:15:25 -0700

    Cleaned up the code for presentation

--- public/newButterfly.js
+++ public/newButterfly.js
@@ -1,64 +1,71 @@
-import socketIOClient from "socket.io-client";
-const url = "https://butterfly-server-api.herokuapp.com";
-const socket = socketIOClient(url);
+var socket = io("https://butterfly-server-api.herokuapp.com");
 
-export function createBlueButterflies() {
+var funcObj = {
+  Purple: createPurpleButterflies,
+  Blue: createBlueButterflies,
+  Orange: createOrangeButterflies,
+  Yellow: createYellowButterflies,
+  Pink: createPinkButterflies,
+};
+
+function clickEmit(callback) {
+  socket.emit("butterfly", callback);
+}
+
+socket.on("butterfly", function (data) {
+  //console.log("Clients: ", funcObj[data])
+  var callback = funcObj[data];
+  callback();
+});
+
+function fiveSecondRule(parentNode, childNode) {
+  setTimeout(function () {
+    parentNode.removeChild(childNode);
+  }, 5000);
+}
+
+function createBlueButterflies() {
   const BlueButton = document.getElementById("Blue");
   const newImg = document.createElement("img");
   newImg.setAttribute("src", "static/BlueButterfly.png");
   newImg.setAttribute("id", "BlueButterflies");
   BlueButton.appendChild(newImg);
-  console.log("Blue: ", newImg);
-}
 
-export function createPinkButterflies() {
-  // const emittedObject = {};
-  // socket.emit("butterfly", {
-  //   pathName: emittedObject,
-  // });
-  const PinkButton = document.getElementById("Pink");
-  const newImg = document.createElement("img");
-  newImg.setAttribute("src", "static/PinkButterfly.png");
-  newImg.setAttribute("id", "PinkButterflies");
-  PinkButton.appendChild(newImg);
-  console.log("Pink: ", newImg);
+  fiveSecondRule(BlueButton, newImg);
 }
-
-export function createOrangeButterflies() {
-  // const emittedObject = {};
-  // socket.emit("butterfly", {
-  //   pathName: emittedObject,
-  // });
-  const OrangeButton = document.getElementById("Orange");
-  const newImg = document.createElement("img");
-  newImg.setAttribute("src", "static/OrangeButterfly.png");
-  newImg.setAttribute("id", "OrangeButterflies");
-  OrangeButton.appendChild(newImg);
-  console.log("Orange: ", newImg);
-}
-
-export function createPurpleButterflies() {
-  // const emittedObject = {};
-  // socket.emit("butterfly", {
-  //   pathName: emittedObject,
-  // });
-  const PurpleButton = document.getElementById("Purple");
-  const newImg = document.createElement("img");
-  newImg.setAttribute("src", "static/PurpleButterfly.png");
-  newImg.setAttribute("id", "PurpleButterflies");
-  PurpleButton.appendChild(newImg);
-  console.log("Purple: ", newImg);
-}
-
-export function createYellowButterflies() {
-  // const emittedObject = {};
-  // socket.emit("butterfly", {
-  //   pathName: emittedObject,
-  // });
+function createYellowButterflies() {
   const YellowButton = document.getElementById("Yellow");
   const newImg = document.createElement("img");
   newImg.setAttribute("src", "static/YellowButterfly.png");
   newImg.setAttribute("id", "YellowButterflies");
   YellowButton.appendChild(newImg);
-  console.log("Yellow: ", newImg);
+
+  fiveSecondRule(YellowButton, newImg);
+}
+function createOrangeButterflies() {
+  const OrangeButton = document.getElementById("Orange");
+  const newImg = document.createElement("img");
+  newImg.setAttribute("src", "static/OrangeButterfly.png");
+  newImg.setAttribute("id", "OrangeButterflies");
+  OrangeButton.appendChild(newImg);
+
+  fiveSecondRule(OrangeButton, newImg);
+}
+function createPinkButterflies() {
+  const PinkButton = document.getElementById("Pink");
+  const newImg = document.createElement("img");
+  newImg.setAttribute("src", "static/PinkButterfly.png");
+  newImg.setAttribute("id", "PinkButterflies");
+  PinkButton.appendChild(newImg);
+
+  fiveSecondRule(PinkButton, newImg);
+}
+function createPurpleButterflies() {
+  const PurpleButton = document.getElementById("Purple");
+  const newImg = document.createElement("img");
+  newImg.setAttribute("src", "static/PurpleButterfly.png");
+  newImg.setAttribute("id", "PurpleButterflies");
+  PurpleButton.appendChild(newImg);
+
+  fiveSecondRule(PurpleButton, newImg);
 }
