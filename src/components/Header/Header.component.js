0f1209328b2cb9e7000bbb14e5a7eeb59175ab47
Katharine Francis <katie-francis@hotmail.com>
2020-05-16 16:44:48 -0700

    Finishing touches to Header styles

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,16 +1,34 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { Col, Row } from "reactstrap";
 
 const Header = ({ handwashing }) => {
   return (
-    <header className={styles.containerFlex}>
-      <h1 id={styles.header}>We are with you.</h1>
+    <div>
+      <Row className={styles.containerFlex}>
+        <Col>
+          <p id={styles.header}>We are with you.</p>
+        </Col>
 
-      <figure style={{ textAlign: "center", paddingTop: "15px" }}>
-        <img src={handwashing} id={styles.handwashing} alt="Handwashing"></img>
-        <figcaption id="subheader">We will overcome COVID 19</figcaption>
-      </figure>
-    </header>
+        <Col>
+          <figure style={{ textAlign: "center", paddingTop: "15px" }}>
+            <img
+              src={handwashing}
+              id={styles.handwashing}
+              alt="Handwashing"
+            ></img>
+            <figcaption id={styles.subheader}>
+              We will overcome COVID 19
+            </figcaption>
+          </figure>
+        </Col>
+      </Row>
+      <Row className={styles.optionalRow}>
+        <div>
+          <p id={styles.optionalParagraph}>We will overcome COVID 19</p>
+        </div>
+      </Row>
+    </div>
   );
 };
 
