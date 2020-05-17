f73606d880a34b55cbcba8c03389b31fa8657889
Katharine Francis <katie-francis@hotmail.com>
2020-05-16 17:54:23 -0700

    Fixed footer

--- src/App.js
+++ src/App.js
@@ -2,28 +2,29 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header.component.js";
 import ButterflyField from "./components/ButterflyField/ButterflyField.component";
 import Form from "./components/Form/Form.component";
-import Footer from './components/Footer/Footer.component';
-import InfoModal from './components/Modal/Modal.component';
+import Footer from "./components/Footer/Footer.component";
+import InfoModal from "./components/Modal/Modal.component";
 
 import handwashing from "./feature_butterflies/handwashing.svg";
+import "./App.css";
 
 const App = () => {
-
   const [isModalOpen, setModalState] = useState([true]);
 
   const toggleModal = () => {
-    setModalState(!isModalOpen)
-  }
+    setModalState(!isModalOpen);
+  };
 
   return (
-    <div style={{ textAlign: "center" }}>
-
-      {isModalOpen ? <InfoModal toggleModal={toggleModal}/> : ""}
+    <div id="container" style={{ textAlign: "center" }}>
+      {isModalOpen ? <InfoModal toggleModal={toggleModal} /> : ""}
 
       <Header handwashing={handwashing} />
       <ButterflyField />
-      <Form />
-      <Footer toggleModal={toggleModal} isModalOpen={isModalOpen} />
+      <div className="footer">
+        <Form />
+        <Footer toggleModal={toggleModal} isModalOpen={isModalOpen} />
+      </div>
     </div>
   );
 };
