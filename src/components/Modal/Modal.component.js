66f961aefbd443b7972dee65072a1261f21266ad
Katharine Francis <katie-francis@hotmail.com>
2020-05-18 10:42:32 -0700

    Updated Modal content and responsiveness for wider screens

--- src/components/Modal/Modal.component.js
+++ src/components/Modal/Modal.component.js
@@ -13,21 +13,13 @@ const InfoModal = ({ toggleModal }) => {
         </div>
         <div className={styles.modalBody}>
           <p>
-            This website was built with the intention of supporting healthcare
-            workers and essential workers worldwide who are fighting the
-            COVID-19 pandemic and risking their health to support the rest of us
-            during this time. We want to let them know that we think of them
-            often and are grateful for all the work that they do.
+            Please help us support our medical professionals and essential
+            workers during this time of crisis. Each butterfly is a button that
+            when clicked displays a <em>flying butterfly </em>
+            representing a special message of gratitude. We believe that showing
+            appreciation is the least we can do. So if you're stuck at home and
+            wish you could do more, just click. And then keep clicking!
           </p>
-          {/* <p>
-            Each butterfly is a button that when clicked displays a butterfly
-            representing the messages below the buttons. To be a part of this
-            butterfly gratitude movement, all you have to do is click a button
-            as much as you like for as long as you like. We believe that showing
-            appreciation during this time is the least we could do so if you
-            feel like you wish you could do more, just click. And then keep
-            clicking.
-          </p> */}
         </div>
         <div className={styles.modalFooter}>
           <button className={styles.modalButton} onClick={toggleModal}>
